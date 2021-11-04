9925fb1090fe0bf119e5ea845963bf32b5dd2823
Adam Murphy <adsmurphy@hotmail.co.uk>
2021-11-04 16:33:19 +0800

    Request sent to coincap API but failed to print anything

--- deploy/bitcoin_price_predictor.py
+++ deploy/bitcoin_price_predictor.py
@@ -1,5 +1,18 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
 
 st.title("Predict the Price of Bitcoin in 1 Hour's Time")
+
+if st.button('Get Recent Bitcoin Data'):
+    # Get hourly Bitcoin data
+    url = "https://api.coincap.io/v2/assets/bitcoin/history?interval=h1"
+
+    payload={}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
+    print('Success')
