f124ff0a9b4b15f187ebd62292c6c204f8c991d4
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-04-13 19:25:05 +0800

    Switched from matplotlib to plotly

--- deploy/deploy_app.py
+++ deploy/deploy_app.py
@@ -3,13 +3,11 @@ import pandas as pd
 import numpy as np
 import requests
 import time
-import matplotlib.pyplot as plt
-import seaborn as sns
 from io import BytesIO
+import plotly.express as px
+import tensorflow as tf
 
 from deploy_helpers import get_last_8_days_hourly_bitcoin_data
-
-sns.set()
 
 st.title("Predict the Price of Bitcoin in 1 Hour's Time")
 
@@ -18,13 +16,9 @@ if st.button("Get Recent Bitcoin Data"):
 
     bitcoin
 
-    fig, ax = plt.subplots()
-    bitcoin.plot(x="date", y="price", ax=ax, figsize=plt.figaspect(1 / 2))
+    fig = px.line(bitcoin, x="date", y="price")
 
-    # Workaround needed to totally control matplotlib figsize
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-    st.image(buf)
+    st.plotly_chart(fig)
 
 if st.button("Make Prediction for Next Hour"):
-    st.write("Not yet implemented")
+    pass
