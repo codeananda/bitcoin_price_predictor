2be949c7920dec2f5d8146b8a30d9b5e96464cd6
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-05-16 16:30:26 +0800

    Added processing outline

--- simple_price_predictor/train.py
+++ simple_price_predictor/train.py
@@ -8,3 +8,8 @@ bitcoin = get_crypto_data("BTC/USDT", "2008-01-01", "2022-05-15")
 bitcoin = bitcoin.close
 
 # Processing steps to take
+# 1. Scale - scale_train_val - log_and_range_0_1 the best
+# 2. Transform to Keras input - timeseries_to_keras_input
+
+# Use tf.keras.utils.timeseries_dataset_from_array ?
+# Helpful article https://www.tensorflow.org/tutorials/structured_data/time_series
