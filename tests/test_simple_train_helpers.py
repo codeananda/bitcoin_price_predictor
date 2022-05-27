13e7fca6448bd4f0deaaa34cc307af1a57535ec2
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-05-27 16:38:46 +0800

    Added test_load_bitcoin_df

--- tests/test_simple_train_helpers.py
+++ tests/test_simple_train_helpers.py
@@ -1,6 +1,11 @@
 from simple_price_predictor.constants import HISTORICAL_BITCOIN_CSV_FILEPATH
+from simple_price_predictor.train_helpers import load_bitcoin_df
 import os
 
 
 def test_historical_bitcoin_csv_filepath_exists():
     assert HISTORICAL_BITCOIN_CSV_FILEPATH.exists()
+
+
+def test_load_bitcoin_df():
+    load_bitcoin_df()
