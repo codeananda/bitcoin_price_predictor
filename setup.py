22791fc80b67519bcbd420287421649dd8785725
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-05-27 15:36:46 +0800

    Added ability to install as package to enable absolute imports with ease

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simple_price_predictor",
+    packages=find_packages(),
+    # list your dependencies, they will be installed when installing this
+    # package
+    install_requires=[],
+    # optional dependencies
+    extras_require={},
+)
