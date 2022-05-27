3f1f48002939c735e7290e8508d8a0284a6ebfd7
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-05-27 17:02:37 +0800

    Added test_raw_bitcoin_data integration test

--- simple_price_predictor/train_helpers.py
+++ simple_price_predictor/train_helpers.py
@@ -4,6 +4,14 @@ import os
 
 
 def load_bitcoin_df():
+    """Load the downloads/price.csv dataset of historical BTC data from 2010-2021
+    and return as a dataframe without an NaN values.
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe of BTC close data from 2010-2021
+    """
     bitcoin = pd.read_csv(
         HISTORICAL_BITCOIN_CSV_FILEPATH,
         index_col=0,
@@ -13,4 +21,12 @@ def load_bitcoin_df():
         header=0,
     )
     bitcoin = bitcoin.dropna()
+    test_raw_bitcoin_data(bitcoin)
     return bitcoin
+
+
+def test_raw_bitcoin_data(bitcoin):
+    """Integration test for loading raw bitcoin data from disk."""
+    assert bitcoin.price.isna().sum() == 0
+    assert bitcoin.price.min() > 0
+    assert len(bitcoin.columns) == 1
