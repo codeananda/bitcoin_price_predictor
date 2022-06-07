a540bcf8a67268756bafa91158bc0d28551b7a9d
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-06-07 14:46:14 +0800

    Added test scaffold structure for make_tf_dataset()

--- tests/test_simple_train_helpers.py
+++ tests/test_simple_train_helpers.py
@@ -1,6 +1,12 @@
 from simple_price_predictor.constants import HISTORICAL_BITCOIN_CSV_FILEPATH
-from simple_price_predictor.train_helpers import load_raw_bitcoin_df
+from simple_price_predictor.train_helpers import (
+    load_raw_bitcoin_df,
+    make_tf_dataset,
+)
 import os
+import tensorflow as tf
+import numpy as np
+import pytest
 
 
 class TestLoadRawBitcoinDF:
@@ -15,3 +21,17 @@ class TestLoadRawBitcoinDF:
         assert bitcoin.price.isna().sum() == 0
         assert bitcoin.price.min() > 0
         assert len(bitcoin.columns) == 1
+
+
+class Test_make_tf_dataset:
+    def test_runs(self):
+        pass
+
+    def test_array_greater_than_2D(self):
+        pass
+
+    def test_array_less_than_2D(self):
+        array_1D = np.arange(100)
+
+        with pytest.raises(ValueError) as exec_info:
+            make_tf_dataset(array_1D, 5, 5, 5)
