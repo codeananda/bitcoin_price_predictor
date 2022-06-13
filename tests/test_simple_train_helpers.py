6e3c3a8e5d578cdaa9c7ff3cb268468d7fd4f902
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-06-13 15:07:30 +0300

    Added test_output_seq_length_is_zero and test_input_seq_length_equals_len_input_array_inference_use

--- tests/test_simple_train_helpers.py
+++ tests/test_simple_train_helpers.py
@@ -7,6 +7,7 @@ import os
 import tensorflow as tf
 import numpy as np
 import pytest
+from pytest_cases import parametrize_with_cases
 
 
 DATASET_SIZE = 1000000
@@ -45,17 +46,13 @@ class Test_make_tf_dataset:
         with pytest.raises(ValueError) as exec_info:
             make_tf_dataset(array_1D, 5, 5, 5)
 
-    def test_zero_output_length(self, input_array_2D):
-        input_seq_length = 200
-        output_seq_length = 0
-        batch_size = 20
-
+    def compare_feature_and_target_shapes(
+        self, array, input_seq_length, output_seq_length, batch_size
+    ):
         expected_feature_shape = (batch_size, input_seq_length, 1)
         expected_target_shape = (batch_size, output_seq_length, 1)
 
-        ds = make_tf_dataset(
-            input_array_2D, input_seq_length, output_seq_length, batch_size
-        )
+        ds = make_tf_dataset(array, input_seq_length, output_seq_length, batch_size)
 
         for x in ds.take(1):
             feature, target = x
@@ -63,5 +60,26 @@ class Test_make_tf_dataset:
         assert expected_feature_shape == feature.shape
         assert expected_target_shape == target.shape
 
-    def test_single_batch(self):
-        pass
+    def test_output_seq_length_is_zero(self, input_array_2D):
+        input_seq_length = 200
+        output_seq_length = 0
+        batch_size = 20
+
+        self.compare_feature_and_target_shapes(
+            input_array_2D, input_seq_length, output_seq_length, batch_size
+        )
+
+    def test_input_seq_length_equals_len_input_array_inference_use(self):
+        """Inputs are as they will be in a single-step inference environment:
+        output_seq_length = 0, batch_size = 1, and input_seq_length is
+        equal to the length of the input dataset.
+        """
+        input_seq_length = 200
+        output_seq_length = 0
+        batch_size = 1
+        # Just contains input_seq_length elements
+        array_single_batch = np.arange(input_seq_length).reshape(-1, 1)
+
+        self.compare_feature_and_target_shapes(
+            array_single_batch, input_seq_length, output_seq_length, batch_size
+        )
