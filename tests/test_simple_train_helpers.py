3c6a71717b3b10248e045a8720a4d64b8f95c655
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-06-13 12:10:55 +0300

    Added test_array_greater_than_2D

--- tests/test_simple_train_helpers.py
+++ tests/test_simple_train_helpers.py
@@ -24,11 +24,15 @@ class TestLoadRawBitcoinDF:
 
 
 class Test_make_tf_dataset:
-    def test_runs(self):
+    def test_single_batch_input(self):
         pass
 
     def test_array_greater_than_2D(self):
-        pass
+        array_3D = np.arange(100)
+        array_3D = array_3D.reshape(-1, 10, 5)
+
+        with pytest.raises(ValueError) as exec_info:
+            make_tf_dataset(array_3D, 5, 5, 5)
 
     def test_array_less_than_2D(self):
         array_1D = np.arange(100)
