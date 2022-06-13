85155fa67bd17423e68e851505a1d319b18901d0
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-06-13 13:56:16 +0300

    Added test_zero_output_length

--- tests/test_simple_train_helpers.py
+++ tests/test_simple_train_helpers.py
@@ -9,6 +9,14 @@ import numpy as np
 import pytest
 
 
+DATASET_SIZE = 1000000
+
+
+@pytest.fixture
+def input_array_2D():
+    return np.arange(DATASET_SIZE).reshape(-1, 1)
+
+
 class TestLoadRawBitcoinDF:
     def test_historical_bitcoin_csv_filepath_exists(self):
         assert HISTORICAL_BITCOIN_CSV_FILEPATH.exists()
@@ -24,18 +32,36 @@ class TestLoadRawBitcoinDF:
 
 
 class Test_make_tf_dataset:
-
-    dataset_size = 1000000
-
     def test_array_greater_than_2D(self):
-        array_3D = np.arange(self.dataset_size)
+        array_3D = np.arange(DATASET_SIZE)
         array_3D = array_3D.reshape(-1, 10, 5)
 
         with pytest.raises(ValueError) as exec_info:
             make_tf_dataset(array_3D, 5, 5, 5)
 
     def test_array_less_than_2D(self):
-        array_1D = np.arange(self.dataset_size)
+        array_1D = np.arange(DATASET_SIZE)
 
         with pytest.raises(ValueError) as exec_info:
             make_tf_dataset(array_1D, 5, 5, 5)
+
+    def test_zero_output_length(self, input_array_2D):
+        input_seq_length = 200
+        output_seq_length = 0
+        batch_size = 20
+
+        expected_feature_shape = (batch_size, input_seq_length, 1)
+        expected_target_shape = (batch_size, output_seq_length, 1)
+
+        ds = make_tf_dataset(
+            input_array_2D, input_seq_length, output_seq_length, batch_size
+        )
+
+        for x in ds.take(1):
+            feature, target = x
+
+        assert expected_feature_shape == feature.shape
+        assert expected_target_shape == target.shape
+
+    def test_single_batch(self):
+        pass
