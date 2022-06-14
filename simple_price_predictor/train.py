74fee51e5aa3d0e5f4ae2ec5b920c3ff0d69ae1f
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-06-14 17:05:32 +0300

    Added constants at top of file

--- simple_price_predictor/train.py
+++ simple_price_predictor/train.py
@@ -4,7 +4,13 @@ import tensorflow as tf
 from sktime.forecasting.model_selection import temporal_train_test_split
 from sklearn.preprocessing import MinMaxScaler
 
-from simple_price_predictor.train_helpers import load_raw_bitcoin_df, make_tf_dataset
+from simple_price_predictor.train_helpers import (
+    load_raw_bitcoin_df,
+    make_tf_dataset,
+    build_LSTM_training,
+)
+
+from tensorflow.keras.callbacks import EarlyStopping
 
 
 # Processing steps
@@ -17,12 +23,16 @@ from simple_price_predictor.train_helpers import load_raw_bitcoin_df, make_tf_da
 # 2. Get callbacks
 # 3. Fit model
 
+BATCH_SIZE = 20
+OUTPUT_SEQ_LENGTH = 1
+INPUT_SEQ_LENGTH = 200
+
 
 def main():
     bitcoin = load_raw_bitcoin_df()
 
     # In total we have: ~70% training, 20% val, 10% test
-    train, test = temporal_train_test_split(bitcoin, train_size=0.9)
+    train, test = temporal_train_test_split(bitcoin, train_size=0.01)
     train, val = temporal_train_test_split(train, train_size=0.77)
 
     train = np.log(train)
@@ -38,20 +48,35 @@ def main():
     tf_dataset_params = dict()
 
     train_ds = make_tf_dataset(
-        train, input_seq_length=200, output_seq_length=1, batch_size=20
+        train,
+        input_seq_length=INPUT_SEQ_LENGTH,
+        output_seq_length=OUTPUT_SEQ_LENGTH,
+        batch_size=BATCH_SIZE,
     )
     val_ds = make_tf_dataset(
-        val, input_seq_length=200, output_seq_length=1, batch_size=20
+        val,
+        input_seq_length=INPUT_SEQ_LENGTH,
+        output_seq_length=OUTPUT_SEQ_LENGTH,
+        batch_size=BATCH_SIZE,
     )
     test_ds = make_tf_dataset(
-        test, input_seq_length=200, output_seq_length=1, batch_size=1
+        test,
+        input_seq_length=INPUT_SEQ_LENGTH,
+        output_seq_length=OUTPUT_SEQ_LENGTH,
+        batch_size=1,
     )
 
-    # model = build_LSTM_training()
+    model = build_LSTM_training(batch_size=BATCH_SIZE, timesteps=INPUT_SEQ_LENGTH)
 
-    # callbacks = get_callbacks()
+    early_stop_cb = EarlyStopping(patience=10, restore_best_weights=True, baseline=None)
 
-    # history = model.fit()
+    callbacks = [early_stop_cb]
+
+    history = model.fit(
+        train_ds, epochs=10, shuffle=False, validation_data=val_ds, callbacks=callbacks,
+    )
+
+    return history
 
 
 if __name__ == "__main__":
