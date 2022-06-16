1ced748f7aebe65d7c63aa126e4fb988f7936225
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-06-16 18:33:36 +0300

    Moved TF dataset and BATCH_SIZE constants to constants.py

--- simple_price_predictor/train.py
+++ simple_price_predictor/train.py
@@ -11,6 +11,12 @@ from simple_price_predictor.train_helpers import (
     build_LSTM_training,
 )
 
+from simple_price_predictor.constants import (
+    OUTPUT_SEQ_LENGTH,
+    INPUT_SEQ_LENGTH,
+    BATCH_SIZE_TRAINING,
+)
+
 from tensorflow.keras.callbacks import EarlyStopping
 
 
@@ -23,10 +29,6 @@ from tensorflow.keras.callbacks import EarlyStopping
 # 1. Build training model
 # 2. Get callbacks
 # 3. Fit model
-
-BATCH_SIZE = 50
-OUTPUT_SEQ_LENGTH = 1
-INPUT_SEQ_LENGTH = 200
 
 
 def main():
@@ -53,13 +55,13 @@ def main():
         train,
         input_seq_length=INPUT_SEQ_LENGTH,
         output_seq_length=OUTPUT_SEQ_LENGTH,
-        batch_size=BATCH_SIZE,
+        batch_size=BATCH_SIZE_TRAINING,
     )
     val_ds = make_tf_dataset(
         val,
         input_seq_length=INPUT_SEQ_LENGTH,
         output_seq_length=OUTPUT_SEQ_LENGTH,
-        batch_size=BATCH_SIZE,
+        batch_size=BATCH_SIZE_TRAINING,
     )
     test_ds = make_tf_dataset(
         test,
@@ -68,7 +70,9 @@ def main():
         batch_size=1,
     )
 
-    model = build_LSTM_training(batch_size=BATCH_SIZE, timesteps=INPUT_SEQ_LENGTH)
+    model = build_LSTM_training(
+        batch_size=BATCH_SIZE_TRAINING, timesteps=INPUT_SEQ_LENGTH
+    )
 
     early_stop_cb = EarlyStopping(patience=10, restore_best_weights=True, baseline=None)
 
@@ -80,7 +84,7 @@ def main():
         shuffle=False,
         validation_data=val_ds,
         callbacks=callbacks,
-        batch_size=BATCH_SIZE,
+        batch_size=BATCH_SIZE_TRAINING,
     )
 
     model.save("models/basic_LSTM.h5")
