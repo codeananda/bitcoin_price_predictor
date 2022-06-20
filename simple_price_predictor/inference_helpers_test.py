e9013363d337f884839282fb2f283b0ed62a29e5
Adam Murphy <adsmurphy@hotmail.co.uk>
2022-06-20 14:59:09 +0300

    process_coincap_response - added two unit tests

--- simple_price_predictor/inference_helpers_test.py
+++ simple_price_predictor/inference_helpers_test.py
@@ -25,12 +25,12 @@ class MockCoincapAPIResponse:
     def json():
         data = []
         # Choose 1 full day of data for simplicity
-        for i in range(1, 25):
+        for i in range(23, -1, -1):
             row = {
-                "priceUsd": f"{i * 10.}",  #  Must be float
+                "priceUsd": f"{i * 10.25}",  #  Must be float
                 "time": i,  # Dropped in preprocessing
                 "circulatingSupply": f"{i}",  # Dropped during processing
-                "date": f"2022-06-18T{i-1:0>2}:00:00.000Z",  # Must be datetime
+                "date": f"2022-06-18T{i:0>2}:00:00.000Z",  # Must be datetime
             }
             data.append(row)
         response = {
@@ -93,3 +93,27 @@ class Test_get_last_num_days_hourly_bitcoin_data:
     def test_num_days_not_int(self, num_days):
         with pytest.raises(TypeError):
             get_raw_coincap_bitcoin_data(num_days)
+
+
+class Test_process_coincap_response:
+    def test_not_a_dict(self):
+        not_a_dict = [1, 2, 3, 4]
+        with pytest.raises(TypeError):
+            process_coincap_response(not_a_dict)
+
+    def test_runs(self):
+        mock_input = MockCoincapAPIResponse().json()
+        expected_date_range = pd.date_range(
+            start="2022-06-18", end="2022-06-19", freq="1H", inclusive="left", tz="UTC"
+        )
+        expected_data = {
+            "date": reversed(expected_date_range),
+            # Must be to 2 decimal places
+            "price": [i * 10.25 for i in range(23, -1, -1)],
+        }
+        expected_output = pd.DataFrame.from_dict(expected_data)
+        actual_output = process_coincap_response(mock_input)
+
+        pd.testing.assert_frame_equal(
+            expected_output, actual_output, check_column_type=True, check_exact=True,
+        )
